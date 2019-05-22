fc0654ebf99643d85219e184349f51a8ac562503
Ritwik Raha <rtzdzn@gmail.com>
2019-05-22 22:48:09 +0530

    some minor changes

--- demystifier.m
+++ demystifier.m
@@ -3,28 +3,22 @@ I = imread('starry_night.jpg');
 %we read the image and find out it's size and number of channels
 %this is always step 1 for finding out any mysteries in an image 
 size(I);
-red = I(:,:,1); % Red channel
-green = I(:,:,2); % Green channel
-blue = I(:,:,3); % Blue channel
-black = zeros(size(I, 1), size(I, 2));
-just_red = cat(3, red, black, black);
-just_green = cat(3, black, green, black);
-just_blue = cat(3, black, black, blue);
 
 %% Image cropping - eddies
 % the image is cropped to sample the eddies that make up the most
 % interesting part of the image 
 I2 = imread('just_stars.jpg');
 is = size(I2);
-%resizing the original image to use for feature extraction
-I3 = imresize(I,[is(1) is(2)]);
-
+%cropping from the cropped image to use for feature extraction
 % [feat, rect] = imcrop(I2);
 % imwrite(feat,'small_feature.jpg');
+I3 = imread('small_feature.jpg');
 
+[I2r,I2g,I2b]=channelsplitter(I2);
+[I3r,I3g,I3b]=channelsplitter(I3);
 %% Image correlation
 
-corr = real(ifft2(fft2(rgb2gray(I2)) .* fft2(rot90(rgb2gray(feat),2),is(1),is(2))));
+corr = real(ifft2(fft2(rgb2gray(I2)) .* fft2(rot90(rgb2gray(I3),2),is(1),is(2))));
 figure
 imshow(corr,[])
 
@@ -48,4 +42,4 @@ Td = corr > thresh;
 se = strel('disk',5);
 Tdil = imdilate(Td,se);
 figure
-imshow(Tdil) 
+imshow(Tdil,[]) 
